7691e591918cef38c9c53d5b4ac29013d4be261f
Petar Susac <petar.susac@fer.hr>
2025-07-09 14:57:35 +0200

    Trigger phonation warning only if during speech

--- speech_warnings.py
+++ speech_warnings.py
@@ -13,6 +13,7 @@ def phonation_warning(features: PlotData, threshold_duration=20, threshold_num=1
     short_intervals = 0
     if features.phonation_intervals:
         for interval in features.phonation_intervals:
-            if (interval[1] - interval[0]) < threshold_duration and interval[0] != 0 and interval[1] != len(features.zcr):
+            if (interval[1] - interval[0]) < threshold_duration and features.speech_activity[interval[0]:interval[1]].any() \
+                and interval[0] != 0 and interval[1] != len(features.zcr):
                 short_intervals += 1
     return short_intervals >= threshold_num
